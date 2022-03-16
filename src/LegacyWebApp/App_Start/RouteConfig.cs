b4c23aeb459be63a1f523f8adc96c7f93a3816be
Vinny Martins <martins.vds@outlook.com>
2022-03-16 15:35:29 -0600

    replace swagger route with home controller

--- src/LegacyWebApp/App_Start/RouteConfig.cs
+++ src/LegacyWebApp/App_Start/RouteConfig.cs
@@ -13,31 +13,11 @@ namespace LegacyWebApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // Set Swagger as default start page
-            routes.MapHttpRoute(
-                name: "swagger_root",
-                routeTemplate: "",
-                defaults: null,
-                constraints: null,
-                handler: new RedirectHandler((message =>
-                {
-                    if (message.Headers.TryGetValues("X-Forwarded-Proto", out var list))
-                    {
-                        if (list != null && list.Any(l => l.Equals("https", StringComparison.InvariantCultureIgnoreCase)))
-                        {
-                            return message.RequestUri.GetComponents(UriComponents.AbsoluteUri & ~UriComponents.Port, UriFormat.UriEscaped).ToString();
-                        }
-                    }
-                    
-                    return message.RequestUri.ToString();
-                }), "swagger")
-            );
-
             //ASP.NET MVC Route Config
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "swagger", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
